931f728ed3c2f47a7bdc12c2b60a459a534db020
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2023-03-28 19:18:47 +0530

    done

--- abha_auth_test/lib/Routes/routes_name.dart
+++
@@ -1,4 +0,0 @@
-class RoutesName {
-  static const String abhaMobileRoute = '/abha-mobile-login';
-  static const String otpRoute = '/otp-screen';
-}
--- abha_auth_test/lib/constants/url.dart
+++
@@ -1,8 +0,0 @@
-class AppUrl {
-  static const baseUrl = 'https://healthidsbx.abdm.gov.in/api';
-  static const loginWithMobileAbhaEndPoint =
-      '/v2/registration/mobile/login/generateOtp';
-
-  static const otpMobileVerfiyEndPoint =
-      '/v2/registration/mobile/login/verifyOtp';
-}
